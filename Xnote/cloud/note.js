c21836feacf5b662da8ad4decc3d6ad53d80d56d
xuefeng <jerrypeng777@yeah.net>
2015-11-23 18:50:07 -0500

    added note query endpoint

--- Xnote/cloud/note.js
+++ Xnote/cloud/note.js
@@ -5,7 +5,30 @@ module.exports = function (){
 	var express = require('express');
 	var app = express();
 
-  
+  //note query endpoint
+  app.get('/note', function(req, res){
+
+    var subject = req.body.subject;
+
+    var note = Parse.Object.extend("Notes");
+    var query = new Parse.Query(note);
+    //if subject is not provided, send all notes back
+    if(subject !== null){
+      query.equalTo('subject',subject);
+    }
+
+    query.find({
+      success: function(notes) {
+        res.send(notes);
+        return;
+      },
+      error: function(error) {
+        res.send(error);
+        return;
+      }
+    });
+    
+  });
 
   return app;
 
