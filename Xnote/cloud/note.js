a198e9e58783e0293184077ac67d1169e1d7b539
xuefeng <jerrypeng777@yeah.net>
2015-11-23 19:37:30 -0500

    added purchased note endpoint

--- Xnote/cloud/note.js
+++ Xnote/cloud/note.js
@@ -30,6 +30,32 @@ module.exports = function (){
     
   });
 
+  //user purchased note endpoints
+  app.get('/purchased_note'), function(req, res){
+
+    var currentUser = Parse.User.current();
+
+    var subject = req.body.subject;
+    var relation = currentUser.relation("purchased_note");
+    var note_query = relation.query();
+    //if subject is not provided, send all notes back
+    if(subject !== null){
+      note_query.equalTo('subject',subject);
+    }
+
+    note_query.find({
+      success: function(notes) {
+        res.send(notes);
+        return;
+      },
+      error: function(error) {
+        res.send(error);
+        return;
+      }
+    });
+    
+  });
+
   return app;
 
 }();
